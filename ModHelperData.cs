270d44fb16ea59255fdc8bef3c9959333dd9a37f
Stiefanek <107305979+Stiefanek@users.noreply.github.com>
2024-10-17 22:58:51 +0200

    Preparing for V2

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+namespace BakerMonkey;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "45.0";
+    public const string Version = "0.0.1";
+    public const string Name = "BakerMonkey V2 WIP";
+
+    public const string Description = "Slowly Fixes the Tower";
+
+    public const string RepoOwner = "Stiefanek"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "BakerMonkeyV2"; // TODO add your repo name here, also in the download url in README.md
+}
